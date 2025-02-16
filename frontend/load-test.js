f7595a161ee68e27eb1a0878345016e41de2c84b
Chiqo Rifky Saputra <chiqo.rifky@appschef.com>
2025-02-16 15:32:43 +0700

    update duration

--- frontend/load-test.js
+++ frontend/load-test.js
@@ -3,9 +3,9 @@ import { check, sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '30s', target: 20 }, // Ramp-up to 20 users over 30 seconds
-    { duration: '1m', target: 20 },  // Stay at 20 users for 1 minute
-    { duration: '10s', target: 0 },  // Ramp-down to 0 users over 10 seconds
+    { duration: '10s', target: 10 }, // Ramp-up to 10 users over 10 seconds
+    { duration: '20s', target: 10 }, // Stay at 10 users for 20 seconds
+    { duration: '5s', target: 0 },   // Ramp-down to 0 users over 5 seconds
   ],
 };
 
